80447dcada664a0019fad76de9832746b21a6134
Bilal Siddiqi <150631923+bilalsiddiqi629@users.noreply.github.com>
2025-07-11 19:20:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import asyncio
 import webserver
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")  
+TOKEN = os.environ['discordkey']
 
 class Volunteer:
     def __init__(self, username):
@@ -167,4 +167,4 @@ async def send_message_daybefore(ctx, volunteer, hour, int):
         await ctx.send(f"Hey, {volunteer.username}! You're on shift for set up the day before the event. Please be on time for your shift.")
         
 webserver.keep_alive()
-bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
+bot.run(TOKEN)
